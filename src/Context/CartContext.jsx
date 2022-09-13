160abd732a0a3eb3ea9cecaff933b5ec98afacaa
Lucas Cabral <lucascabral6008@gmail.com>
2022-09-13 11:30:34 -0300

    swal message

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -40,13 +40,13 @@ export const CartProvider = ({children}) => {
 
     const emptyCart = () => {
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            title: '¿Estás seguro?',
+            text: "No serás capaz de revertir esto!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Si, eliminalo!'
           }).then((result) => {
             if (result.isConfirmed) {
                 setCart([])
